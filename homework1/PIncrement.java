b67d8339c2bcc4b6e32cf23bb752789b2681e11d
Carter Airhart <airhartcarter@utexas.edu>
2018-09-11 11:43:18 -0500

    added suupport for reentrantlock

--- homework1/PIncrement.java
+++ homework1/PIncrement.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class PIncrement{
 	public static int parallelIncrement(int c, int numThreads){
@@ -19,11 +21,12 @@ public class PIncrement{
 				threadListAtomic[listLoc].join();
 			}
 			// End Time Atomic
-
+			
 			Integer synchronizedInt = new Integer(0);
 			SynchronizedIncrement[] threadListSynchronized =
 							new SynchronizedIncrement[i];
 
+			// Start Timer Synchronized
 			for(int j = 0; j < i; j++){
 				int incrementBy = (j < remainder) incrementPerThread+1 :
 															 incrementPerThread;
@@ -35,6 +38,26 @@ public class PIncrement{
 			for(int j = 0; j < i; j++){
 				threadListSynchronized[j].join();	
 			}
+			// End Timer Synchronized
+
+			private final ReentrantLock lock = new ReentrantLock();
+			Integer reentrantInt = new Integer(0);
+			ReentrantIncrement[] threadListReentrant = 
+							new ReentrantIncrement[i];
+
+			// Start Time Reentrant Lock
+			for(int j = 0; j < i; j++){
+				int incrementBy = (j < remainder) incrementPerThread+1 :
+															 incrementPerThread;
+				spinupReentrant(reentrantInt, incrrementBy,
+										threadListReentrant, j);
+			}
+
+			for(int j = 0; j < i; j++){
+				threadListReentrant[j].join();
+			}
+			// End Time Reentrant Lock
+
 
 		}
 	}
@@ -52,6 +75,15 @@ public class PIncrement{
 														SynchronizedIncrement[] threadList,
 														int listLoc){
 		threadList[listLoc] = new SynchronizedIncrement(c, incrementNumber);
+		threadList[listLoc].start();
+	}
+
+	private static void spinupReentrant(Integer c,
+													int incrementNumber,
+													ReentrantLock lock,
+													ReentrantIncrement[] threadList,
+													int listLoc){
+		threadList[listLoc] = new ReentrantIncrement(c, incrementNumber, lock);
 		threadList[listLoc].start();
 	}
 }
@@ -88,6 +120,31 @@ class SynchronizedIncrement extends Thread{
 		for(int i = 0; i < incrementNumber; i++){
 			synchronized(this){
 				c++;	
+			}
+		}
+	}
+}
+
+class ReentrantIncrement extends Thread{
+	private Integer c;
+	private int incrementNumber;
+	private ReentrantLock lock;
+
+	public ReentrantIncrement(Integer c, int incrementNumber, ReentrantLock lock){
+		this.c = c;
+		this.incrementNumber = incrementNumber;
+		this.lock = lock;
+	}
+
+	@Override
+	public void run(){
+		for(int i = 0; i < incrementNumber; i++){
+			lock.lock();
+			try{
+				c++;
+			}
+			finally{
+				lock.unlock();
 			}
 		}
 	}
