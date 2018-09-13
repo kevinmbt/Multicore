fbb4d091ef2a3dc4f71ace4cd2128a5e48651694
Carter Airhart <airhartcarter@utexas.edu>
2018-09-13 12:34:49 -0500

    verified and added unit tests

--- homework1/PIncrement.java
+++ homework1/PIncrement.java
@@ -1,65 +1,96 @@
 import java.util.*;
-
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
 
 public class PIncrement{
+	public static void main(String args[]){
+		for(int i = 1; i <=8; i++){
+			parallelIncrement(12000000, i);	
+		}
+	}
 	public static int parallelIncrement(int c, int numThreads){
-		for(int i = 1; i <= 8; i++){
 			int incrementPerThread = c/numThreads;
 			int remainder = c%numThreads;
 
 			AtomicInteger atomic = new AtomicInteger(0);
-			AtomicIncrement[] threadListAtomic = new AtomicIncrement[i];
+			AtomicIncrement[] threadListAtomic = new AtomicIncrement[numThreads];
 			
 			// Start Timer Atomic
-			for(int j = 0; j < i; j++){
-				int incrementBy = (j < remainder) incrementPerThread+1 :
+			long atomicstart = System.nanoTime();
+			for(int j = 0; j < numThreads; j++){
+				int incrementBy = (j < remainder)?  incrementPerThread+1 :
 															 incrementPerThread;
 				spinupAtomic(atomic, incrementBy, threadListAtomic, j); 	
 			}
 
-			for(int j = 0; j < i; j++){
-				threadListAtomic[listLoc].join();
+			for(int j = 0; j < numThreads; j++){
+				try{
+					threadListAtomic[j].join();
+				}
+				catch(Exception e){
+					
+				}
 			}
+			long atomicend = System.nanoTime();
 			// End Time Atomic
+			System.out.println("Atomic Time for " +numThreads+ " threads:: " +
+								(atomicend-atomicstart));
 			
 			Integer synchronizedInt = new Integer(0);
 			SynchronizedIncrement[] threadListSynchronized =
-							new SynchronizedIncrement[i];
+							new SynchronizedIncrement[numThreads];
 
 			// Start Timer Synchronized
-			for(int j = 0; j < i; j++){
-				int incrementBy = (j < remainder) incrementPerThread+1 :
+			long	 synchronizedstart = System.nanoTime();
+			for(int j = 0; j < numThreads; j++){
+				int incrementBy = (j < remainder)? incrementPerThread+1 :
 															 incrementPerThread;
 				spinupSynchronized(synchronizedInt, incrementBy,
 											threadListSynchronized, j);
 
 			}
 
-			for(int j = 0; j < i; j++){
-				threadListSynchronized[j].join();	
+			for(int j = 0; j < numThreads; j++){
+				try{	
+					threadListSynchronized[j].join();	
+				}
+				catch(Exception e){
+					
+				}
 			}
+			long synchronizedend = System.nanoTime();
 			// End Timer Synchronized
-
-			private final ReentrantLock lock = new ReentrantLock();
+			System.out.println("Synchronized Time for " +numThreads+ " threads:: " +
+								(synchronizedend-synchronizedstart));
+			final ReentrantLock lock = new ReentrantLock();
 			Integer reentrantInt = new Integer(0);
 			ReentrantIncrement[] threadListReentrant = 
-							new ReentrantIncrement[i];
+							new ReentrantIncrement[numThreads];
 
 			// Start Time Reentrant Lock
-			for(int j = 0; j < i; j++){
-				int incrementBy = (j < remainder) incrementPerThread+1 :
+			long reentrantstart = System.nanoTime();
+			for(int j = 0; j < numThreads; j++){
+				int incrementBy = (j < remainder)? incrementPerThread+1 :
 															 incrementPerThread;
-				spinupReentrant(reentrantInt, incrrementBy,
-										threadListReentrant, j);
+				spinupReentrant(reentrantInt, incrementBy,
+									lock, threadListReentrant, j);
 			}
 
-			for(int j = 0; j < i; j++){
-				threadListReentrant[j].join();
+			for(int j = 0; j < numThreads; j++){
+				try{
+					threadListReentrant[j].join();
+				}
+				catch(Exception e){
+						
+				}
 			}
+			long reentrantend = System.nanoTime();
+			System.out.println("Reentrant Time for " +numThreads+ " threads:: " +
+								(reentrantend-reentrantstart));
+
 			// End Time Reentrant Lock
+		return 0;
 
-
-		}
 	}
 
 	private static void spinupAtomic(AtomicInteger c,
@@ -98,7 +129,7 @@ class AtomicIncrement extends Thread{
 	}
 
 	@Override
-	public void run{
+	public void run(){
 		for(int i = 0; i < incrementNumber; i++){
 			int expected = c.get();
 			c.compareAndSet(expected, expected+1);
